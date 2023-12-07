1c058cab585916d03ae65d3f2eb5ee4f6c50ff00
=jahanzaib-div <jahanzaibmaqbool0307@gmail.com>
2023-12-07 17:15:12 +0500

    javascript variable

---
+++ Variable.js
@@ -0,0 +1,31 @@
+// 4 method to declare varable in javascript
+// 1 automattically
+// 2 var keyword
+// 3 let keyword
+// 4 const keyword
+
+x =10;
+y =20;
+z=x+y;
+console.log(z)
+
+// var keyword
+//var keyword was used in all JavaScript code from 1995 to 2015.
+ var a =20;
+ var b=30;
+ var c = a+b;
+
+ console.log(c)
+
+ //let and const keywords were added to JavaScript in 2015.
+ let m = 20;
+ let n = 40;
+ let p = m+n;
+ console.log(p)
+
+
+ // const keyword
+ const num1= 345;
+ const num2 =345;
+ const sum = num1+num2;
+ console.log(sum)
