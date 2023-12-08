7085a94a5bb550188c806d624f966d4c3acda537
=jahanzaib-div <jahanzaibmaqbool0307@gmail.com>
2023-12-09 00:20:57 +0500

    DataTypes in JavaScript

---
+++ DataTypes.js
@@ -0,0 +1,46 @@
+// javascript has 8 datatypes
+// 1 String 
+// 2 Number 
+// 3 Bigint
+// 4 Boolean
+// 5 Undefined
+// 6 Null
+// 7 Symbol
+// 8 object 
+
+// object datatype contain 
+// 1 an array 
+// 2 an object
+// 3 A date 
+
+//  String 
+let color = "red";
+let name = "xyz";
+
+// string concat   output red xyz
+console.log(color + name)
+
+let Number_1 = 235;
+let Number_2 = 23.4;
+console.log(Number_1+Number_2)
+// Boolean
+let x = true
+let y = false
+console.log(x)
+console.log(y)
+// object
+
+const person = {name : "abc", age: 23};
+
+// array
+const car = ["audi","bmw","saad"];
+
+console.log(car)
+
+
+// date
+ const date = new Date("12-09-2023\t")
+
+ console.log(date)
+
+
