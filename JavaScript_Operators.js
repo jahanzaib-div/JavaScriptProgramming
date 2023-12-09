1179575509440fe063c9ca1863139eb077200e64
=jahanzaib-div <jahanzaibmaqbool0307@gmail.com>
2023-12-09 17:04:08 +0500

    Operators in javascript

---
+++ JavaScript_Operators.js
@@ -0,0 +1,61 @@
+//Types of JavaScript Operators
+// 1 Arithmetic Operators
+// 2 Assignment Operators
+// 3 comparison Operators
+// 4 String Operators
+// 5 Logical Operators
+// 6 Bitwise Operators
+// 7 Ternary Opertors
+// 8 Type Operators
+
+// 1 Arithmetic Operators
+ let a = 20;
+ let b = (100+80) +a;
+ console.log(b)   // output 200
+
+ let c= 20;
+ let d = 20;
+ console.log(c*d);  // output 400
+
+ // Exponentiation
+ console.log(2**3);   //output 8
+
+ // division
+ console.log(c/d);   // output 1
+ // Modulus (Division Remainder)
+
+console.log(c%2);  // output 0
+
+
+
+// JavaScript Comparison Operators
+// equal to  ==
+// equal value and equal type  ===
+// not equal   !=
+// not equal andequal type  !==
+// grater then   >
+// less then     <
+// less then equal <=
+// grater then   >=
+// ternary operator ?
+
+
+let text1 = "20"
+let text2 = "1"
+let result = text1<text2;
+console.log(result);
+// string are compared alphabatically
+
+
+//string addition
+let text4 = "John";
+let text5 = "Doe";
+let resul = text4 + " " + text5;
+console.log(resul)
+
+// also concate 
+
+let text6 = "What a very ";
+text6 += "nice day";
+
+console.log(text6)
